fe757accd7678849d493070c75624bb0837ed1af
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2018-08-21 14:53:35 +0900

    modify get_participant.py

--- get_participant.py
+++ get_participant.py
@@ -5,18 +5,40 @@ from datetime import datetime
 import urllib.request, urllib.error
 from bs4 import BeautifulSoup
 
-##-------------------------------------------------------##
-## グループ名とイベント番号を取り出してファイル名とする
-## url          対象イベントのURL
-## extension    拡張子  ex) ".csv"
-##-------------------------------------------------------##
+def print_arg_error(commandName):
+    """引数エラー時のメッセージ出力"""
+    print("Usage: python {cmd} connpassURL\n"
+          "\n"
+          "example)\n"
+          "python {cmd} https://GROUP_NAME.connpass.com/event/xxxxx/"
+          .format(cmd=commandName))
+    exit(0)
+
+
+def is_url(url):
+    """URL書式チェック"""
+    return url.startswith("https://") or url.startswith("http://")
+
+
+def participation_url(url):
+    """参加者URLに変換"""
+    if url.endswith("/participation/"):
+        return url
+    if url.endswith("/participation"):
+        return url + "/"
+    if url.endswith("/"):
+        return url + "participation/"
+    else:
+        return url + "/participation/"
+
+
 def getgrpName(url, extention):
-    # 現在の時刻を年、月、日、時、分、秒で取得します
-    dateStr = datetime.now().strftime("%Y%m%d_%H%M%S")
+    """グループ名とイベント番号を取り出してファイル名とする"""
 
-    # url = "https://<groupName>.connpass.com/event/<event_id>/participation/"
+    # url format
+    # "https://<groupName>.connpass.com/event/<eventId>/participation/"
 
-    # グループ名<groupName>を取り出す
+    # <groupName>を取り出す
     pos_l1 = url.find("/")
     pos_l2 = url.find("/", pos_l1 + 1)
     pos_l3 = url.find(".")
@@ -25,52 +47,57 @@ def getgrpName(url, extention):
         print("groupName error...")
         return ""
 
-    groupname = url[pos_l2 + 1:pos_l3]
+    groupName = url[pos_l2 + 1:pos_l3]
 
-    # イベント番号<event_id>を取り出す
+    # <eventId>を取り出す
     pos_r1 = url.rfind("/")
     pos_r2 = url.rfind("/", 0, pos_r1 - 1)
     pos_r3 = url.rfind("/", 0, pos_r2 - 1)
 
     if ((pos_r1 < 0) or (pos_r2 < 0) or (pos_r3 < 0)):
-        print("event_id error...")
+        print("eventId error...")
         return ""
 
-    eventid = url[pos_r3 + 1:pos_r2]
+    eventId = url[pos_r3 + 1:pos_r2]
 
-    return(groupname + "_" + eventid + "_" + dateStr + extention)
+    # 現在の時刻を年、月、日、時、分、秒で取得
+    dateStr = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-##-------------------------------------------------------##
-## メイン
-##-------------------------------------------------------##
-if __name__ == "__main__":
+    return(groupName + "_" + eventId + "_" + dateStr + extention)
+
+
+def users(tags):
+    """ユーザー情報の取得(ユーザ名とユーザURLのCSV形式)"""
+    for tag in tags:
+        try:
+            if tag.text and tag.text != "\n\n":
+                href = tag['href']
+                if "/open/" in href or "/user/" in href:
+                    yield '"{name}","{url}"\n'.format(name=tag.text, url=href)
+        except:
+            pass
+
+
+def main():
     args = sys.argv
-    argc = len(args)
 
-    if argc <= 1:
-        print("Usage: python %s connpassURL" %(args[0]))
-        exit(0)
+    if len(args) <= 1:
+        print_arg_error(args[0])
 
-    # アクセスするURL
-    # url = "https://xxxxxx.connpass.com/event/xxxxx/participation/"
-    if ((args[1].rfind("https://") < 0) and (args[1].rfind("http://") < 0)):
-        print("Usage: python %s connpassURL" %(args[0]))
-        print("ex) > python %s https://xxxxxx.connpass.com/event/xxxxx/" %(args[0]))
-        exit(0)
+    # URL Format
+    # "https://xxxxxx.connpass.com/event/xxxxx/"
 
-    if (args[1].rfind("/participation/") >= 0):
-        url = args[1]
-    elif (args[1].rfind("/participation") >= 0):
-        url = args[1] + "/"
-    else:
-        if (args[1].endswith("/")):
-            url = args[1] + "participation/"
-        else:
-            url = args[1] + "/participation/"
+    # URL書式チェック
+    if not is_url(args[1]):
+        print("URL Error...%s\n" %args[1])
+        print_arg_error(args[0])
+
+    # 参加者URLに変換
+    url = participation_url(args[1])
 
     save_fname = getgrpName(url, ".csv")
     if (save_fname == ""):
-        exit
+        print_arg_error(args[0])
 
     # 指定したURLの出力htmlを取得する 
     html = urllib.request.urlopen(url)
@@ -79,24 +106,10 @@ if __name__ == "__main__":
     soup = BeautifulSoup(html, "html.parser")
 
     # すべての<A>タグを抽出する
-    td = soup.find_all("a")
+    tags = soup.find_all("a")
 
-    # ユーザー情報保存用配列
-    ulist = []
-
-    for tag in td:
-        try:
-            if ((tag.text != "") and (tag.text != "\n\n")):
-
-                #print("%s,%s" %(tag.text, tag['href']))
-                hrefStr = tag['href']
-
-                if ((hrefStr.find("/open/") >= 0) or (hrefStr.find("/user/") >= 0)):
-                    # csv形式でリストに出力
-                    ulist.append("\"" + tag.text + "\",\"" + hrefStr +"\"\n")
-
-        except:
-            pass
+    # ユーザー情報保存用リストに格納
+    ulist = list(users(tags))
 
     # 抽出したリストを標準出力に表示する
     for user in ulist:
@@ -107,3 +120,6 @@ if __name__ == "__main__":
         f.writelines(ulist)
 
     print("\nsave as ... [%s]" %(save_fname))
+
+if __name__ == "__main__":
+    main()
