ecd5d52436e4fa63cfe575489f0e29d8d4e2cda6
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2018-08-21 16:37:05 +0900

    remove pandas

--- get_duplicate_user.py
+++ get_duplicate_user.py
@@ -1,44 +1,20 @@
-#!python.exe
 # coding: cp932
 
 import sys
 import os
-import pandas
 
-##-----------------------------------------------------------------------##
-## csvファイルを開く
-##-----------------------------------------------------------------------##
-def readCSVfile(fileName):
-    # encodingは、必要に応じて書き換えてください。
-    lines = pandas.read_csv(fileName, encoding="sjis")
+def getDuplicateUsers(users1, users2):
+    """２つのcsv配列を比較し、profile URLが一致するレコードを出力する"""
+    urls2 = {user2.split('","')[1] for user2 in users2}
+    for user1 in users1:
+        name1, url1 = user1.split('","')
+        if url1 in urls2:
+            yield user1
 
-    return lines
-
-##-----------------------------------------------------------------------##
-## ２つのcsv配列を比較し、profile URLが一致するレコードを出力する
-##-----------------------------------------------------------------------##
-def getDuplicateRecord(lines1, lines2):
-    hit = False
-    for i in range(0, len(lines1)):
-        for j in range(0, len(lines2)):
-            # profileのURLが一致しているか調べる
-            if (lines1.values[i][1] == lines2.values[j][1]):
-                hit = True
-                print("\"%s\",\"%s\"" %(lines1.values[i][0], lines1.values[i][1]))
-
-                # 見つかった場合は次のレコードへ
-                break
-
-    return hit
-
-##-----------------------------------------------------------------------##
-## メイン
-##-----------------------------------------------------------------------##
-if __name__ == "__main__":
+def main():
     args = sys.argv
-    argc = len(args)
 
-    if argc <= 2:
+    if len(args) <= 2:
         print("Usage: python %s file1 file2" %(args[0]))
         exit()
 
@@ -50,14 +26,16 @@ if __name__ == "__main__":
         print("%s not found." %(args[2]))
         exit(0)
 
-    lines1 = []
-    lines1 = readCSVfile(args[1])
+    with open(args[1]) as f:
+        users1 = f.readlines()
 
-    lines2 = []
-    lines2 = readCSVfile(args[2])
+    with open(args[2]) as f:
+        users2 = f.readlines()
 
-    print()
-    print("=== Dupulicate users [%s] in [%s] ===" %(args[1], args[2]))
-    getDuplicateRecord(lines1, lines2)
+    # ２つのcsv配列を比較し、profile URLが一致するレコードを出力する
+    print("\n=== Dupulicate users [%s] in [%s] ===" %(args[1], args[2]))
+    for user in getDuplicateUsers(users1, users2):
+        print(user, end="")
 
-    print()
+if __name__ == "__main__":
+    main()
